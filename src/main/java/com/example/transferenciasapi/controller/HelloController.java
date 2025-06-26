11ac65803b179c02a5a5c6da09248968d2b46d2f
andromedacripto17 <andromedacripto17@gmail.com>
2025-06-26 10:17:02 -0400

    Adicione HelloController com endpoint raiz

---
+++ src/main/java/com/example/transferenciasapi/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.transferenciasapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "🚀 API Transferências no ar!";
+    }
+}
